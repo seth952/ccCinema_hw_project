3dfe5055c95b14c8db187873143cac4536f3c5f3
User <user@users-mbp.home>
2019-11-22 18:30:02 +0000

    save function tested films

--- console.rb
+++ console.rb
@@ -1,7 +1,7 @@
 require('pry')
 # require_relative( 'models/ticket' )
 require_relative('models/customer')
-# require_relative( 'models/film' )
+require_relative('models/film')
 
 
 # # Ticket.delete_all()
@@ -30,24 +30,24 @@ customer1.save()
 customer2.save()
 customer3.save()
 
-# film1 = Film.new({
-#   'title' => 'Teenwolf',
-#   'price' => 10
-#   })
-#
-#   film2 = Film.new({
-#     'title' => 'Back to the future',
-#     'price' => 10
-#     })
-#
-#     film3 = Film.new({
-#       'title' => 'Breakfast Club',
-#       'price' => 10
-#       })
-#
-# film1.save()
-# film2.save()
-# film3.save()
+film1 = Film.new({
+  'title' => 'Teenwolf',
+  'price' => 10
+  })
+
+  film2 = Film.new({
+    'title' => 'Back to the future',
+    'price' => 10
+    })
+
+    film3 = Film.new({
+      'title' => 'Breakfast Club',
+      'price' => 10
+      })
+
+film1.save()
+film2.save()
+film3.save()
 
 
 binding.pry()
--- models/film.rb
+++ models/film.rb
@@ -1,9 +1,10 @@
-equire_relative('../db/sql_runner')
+require_relative('../db/sql_runner')
 
-attr_accessor :title, :price
-attr_reader :id
-class Film()
 
+class Film
+
+  attr_accessor :title, :price
+  attr_reader :id
 
 
 def initialize(details)
@@ -13,12 +14,12 @@ def initialize(details)
 
 end
 
-# def save()
-#   sql = "INSERT INTO films (title, price) VALUES ($1, $2) RETURNING id"
-#   values = [@title, @price]
-#   result = SqlRunner.run(sql, values)
-#   @id = result[0]['id'].to_i()
-# end
+def save()
+  sql = "INSERT INTO films (title, price) VALUES ($1, $2) RETURNING id"
+  values = [@title, @price]
+  result = SqlRunner.run(sql, values)
+  @id = result[0]['id'].to_i()
+end
 #
 # def self.all()
 #   sql = "SELECT * FROM films"
@@ -29,28 +30,4 @@ end
 # def self.delete_all()
 #   sql = "DELETE FROM films"
 #   SqlRunner.run(sql)
-# end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
