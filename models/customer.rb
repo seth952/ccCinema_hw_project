1bf9466945d0c879d2a212b8dcc38ad6fb8acb08
User <user@Users-MacBook-Pro.local>
2019-11-23 12:03:34 +0000

    films a cust has seen,cust who went to a film,tested,all good

--- models/customer.rb
+++ models/customer.rb
@@ -42,13 +42,13 @@ require_relative('../db/sql_runner')
 
   end
 
-  # def films()
-  #   sql = "SELECT films.* FROM films INNER JOIN tickets ON tickets.film_id = films.id WHERE customer_id = $1"
-  #   values = [@id]
-  #   films = SqlRunner.run(sql, values)
-  #   result = films.map {|film| Film.new(film)}
-  #   return result;
-  # end
+  def films()
+    sql = "SELECT films.* FROM films INNER JOIN tickets ON tickets.film_id = films.id WHERE customer_id = $1"
+    values = [@id]
+    films = SqlRunner.run(sql, values)
+    result = films.map {|film| Film.new(film)}
+    return result;
+  end
 
 
 
--- models/film.rb
+++ models/film.rb
@@ -39,13 +39,13 @@ class Film
 
   end
 
-  # def customers()
-  #   sql = "SELECT customers.* FROM customers INNER JOIN tickets ON tickets.customer_id = customers.id WHERE film_id = $1"
-  #   values = [@id]
-  #   customers = SqlRunner.run(sql, values)
-  #   result = customers.map {|customer| Customer.new(customer)}
-  #   return result;
-  # end
+  def customers()
+    sql = "SELECT customers.* FROM customers INNER JOIN tickets ON tickets.customer_id = customers.id WHERE film_id = $1"
+    values = [@id]
+    customers = SqlRunner.run(sql, values)
+    result = customers.map {|customer| Customer.new(customer)}
+    return result;
+  end
 
 #
 end
